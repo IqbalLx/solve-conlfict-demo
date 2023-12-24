11afe4a3a60a883d7a4850a552e7de4e252b1cd6
Arif Widianto <arifjozz123@gmail.com>
2023-12-24 12:37:31 +0700

    membuat fungsi bubble sort besert implementasinya

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+void display_list(int *list, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", list[i]);
+    }
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void sort(int *list, int length, int type)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        int temp;
+        for (int j = 0; j < length - 1 - i; j++)
+        {
+            if (type == 1)
+            {
+                if (list[j] < list[j + 1])
+                {
+                    swap(&list[j], &list[j + 1]);
+                }
+            }
+            else
+            {
+                if (list[j] > list[j + 1])
+                {
+                    swap(&list[j], &list[j + 1]);
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[5] = {
+        1,
+        3,
+        4,
+        2,
+        5,
+    };
+
+    printf("\n");
+    printf("Original Array : ");
+    display_list(arr, 5);
+
+    sort(arr, 5, 0);
+    printf("\n");
+    printf("Ascending Sort Result : ");
+    display_list(arr, 5);
+
+    sort(arr, 5, 1);
+    printf("\n");
+    printf("Descending Sort Result : ");
+    display_list(arr, 5);
+
+    return 0;
+}
